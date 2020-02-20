e68b633bee8c93d09e2ade227e75b5015f6f7e59
Barry Low <bearcanrun@gmail.com>
2020-02-19 23:08:24 -0800

    feat(svelte-styled-system-components): setup typographyjs

--- libs/svelte-styled-system-components/src/theme/index.js
+++ libs/svelte-styled-system-components/src/theme/index.js
@@ -1,7 +1,12 @@
 import { writable, derived } from 'svelte/store'
+import { toTheme } from '@theme-ui/typography'
+import stAnnesTheme from 'typography-theme-st-annes'
 
 import basic from './basic'
 import dark from './dark'
+
+const basicTypog = toTheme(stAnnesTheme)
+const darkTypog = toTheme(stAnnesTheme)
 
 function createTheme() {
   const { subscribe, set, update } = writable(basic)
