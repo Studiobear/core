12879a61c829ba1540e3399cfcfe28f2a45294a7
Barry Low <bearcanrun@gmail.com>
2020-02-19 21:44:11 -0800

    feat(svelte-styled-system): extend shorthand constants

--- libs/svelte-styled-system/src/constants.js
+++ libs/svelte-styled-system/src/constants.js
@@ -17,14 +17,34 @@ const shd = 'shadow'
 const grid = 'grid'
 const ap = 'ap'
 const gap = `g${ap}`
+const sp = 'space'
+const end = 'end'
+const fst = 'first'
+const lst = 'last'
+const strt = 'start'
+const cent = 'center'
+const ev = 'evenly'
+const arnd = 'around'
+const btwn = 'between'
+const slf = 'self'
+const str = 'stretch'
+const bsl = 'baseline'
 const row = 'row'
+const rev = 'reverse'
 const col = 'col'
 const color = `${col}or`
 const column = `${col}umn`
 const flex = 'flex'
+const bas = 'basis'
+const no = 'no'
+const sf = 'safe'
+const unsf = `un${sf}`
 const wrap = 'wrap'
+const shr = 'shrink'
+const grow = 'grow'
 const dir = 'dir'
 const direction = `${dir}ection`
+const ord = 'order'
 const template = 'template'
 const align = 'align'
 const justify = 'justify'
@@ -38,7 +58,7 @@ const background = 'background'
 const image = 'image'
 const size = 'size'
 const pos = 'pos'
-const position = 'position'
+const position = `${pos}ition`
 const repeat = 'repeat'
 const attachment = 'attachment'
 
@@ -87,17 +107,45 @@ export const shortHandAttributes = new Map([
   ['bl', [`${brd}-${l}`]],
   ['bx', [`${brd}-${l}`, `${brd}-${r}`]],
   ['by', [`${brd}-${t}`, `${brd}-${b}`]],
-  [shd, [`${bx}-${shd}`]],
+  ['shd', [`${bx}-${shd}`]],
   [align, [`${align}-${items}`]],
+  [content, [`${align}-${content}`]],
   [justify, [`${justify}-${content}`]],
-  [wrap, [`${flex}-${wrap}`]],
-  [`${flex}${dir}`, [`${flex}-${direction}`]],
+  ['slf', [`${align}-${slf}`]],
+  [wrap, [wrap]],
+  ['strt', [strt]],
+  [`${slf}${strt}`, [`${slf}-${strt}`]],
+  [end, [end]],
+  [`${slf}${end}`, [`${slf}-${end}`]],
+  ['bsl', [bsl]],
+  [`${fst}${bsl}`, [`${fst} ${bsl}`]],
+  [`${lst}${bsl}`, [`${lst} ${bsl}`]],
+  ['str', [str]],
+  ['sf', [sf]],
+  ['unsf', [unsf]],
+  ['cent', [cent]],
+  [`${sp}${btwn}`, [`${sp}-${btwn}`]],
+  [`${sp}${arnd}`, [`${sp}-${arnd}`]],
+  [`${sp}${ev}`, [`${sp}-${ev}`]],
+  [`${grow}`, [`${flex}-${grow}`]],
   [direction, [`${flex}-${direction}`]],
+  [`${flex}${wrap}`, [`${flex}-${wrap}`]],
+  [`${flex}${shr}`, [`${flex}-${shr}`]],
+  [`${no}${wrap}`, [`${no}${wrap}`]],
+  [`${wrap}${rev}`, [`${wrap}-${rev}`]],
+  [`${flex}${dir}`, [`${flex}-${direction}`]],
+  [`${flex}${flow}`, [`${flex}-${flow}`]],
+  [`${flex}${bas}`, [`${flex}-${bas}`]],
+  [`${flex}${strt}`, [`${flex}-${strt}`]],
+  [`${flex}${end}`, [`${flex}-${end}`]],
+  ['ord', [ord]],
   [gap, [`${grid}-${gap}`]],
   [`${row}`, [`${grid}-${row}`]],
+  [`${row}${rev}`, [`${row}-${rev}`]],
   [`${row}${gap}`, [`${grid}-${row}-${gap}`]],
   [`${row}-${gap}`, [`${grid}-${row}-${gap}`]],
   [`${column}`, [`${grid}-${col}`]],
+  [`${col}${rev}`, [`${col}-${rev}`]],
   [`${col}${gap}`, [`${grid}-${column}-${gap}`]],
   [`${col}-${gap}`, [`${grid}-${column}-${gap}`]],
   [`${area}`, [`${grid}-${area}`]],
