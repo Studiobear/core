983919713e49d1bb3c5af9057212cc88a76eebb3
Barry Low <bearcanrun@gmail.com>
2020-02-26 18:26:28 -0800

    test(list-baby): initial test setup

---
+++ apps/list-baby/src/__tests__/app.spec.js
@@ -0,0 +1,10 @@
+import { render } from '@testing-library/svelte'
+import App from '../App.svelte'
+
+describe('App', () => {
+  test('should render component correctly', () => {
+    const { getByText } = render(App)
+
+    expect(getByText('Hello world!')).toBeInTheDocument()
+  })
+})
