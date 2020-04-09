0853e5a6ebad3ded337bbfd637eba9b58c4a98fc
Barry Low <bearcanrun@gmail.com>
2020-04-08 23:24:25 -0700

    test: fix yager1 nav + disable cypress in main test run

--- apps/yager1/src/components/index.js
+++ apps/yager1/src/components/index.js
@@ -1,7 +1,5 @@
 import Nav from './Nav.svelte'
 import SSR from './styleSSR.svelte'
-import OverviewBoxGlobal from './OverviewBoxGlobal.svelte'
-import OverviewBoxLocal from './OverviewBoxLocal.svelte'
 import Icons from './Icons.svelte'
 import Icon from './Icon.svelte'
 import Card from './Card.svelte'
@@ -10,18 +8,4 @@ import CardBody from './CardBody.svelte'
 import Overlay from './Overlay.svelte'
 import Loading from './Loading.svelte'
 
-export * from './MyContext'
-export * from './Stats'
-export {
-  Nav,
-  SSR,
-  OverviewBoxGlobal,
-  OverviewBoxLocal,
-  Icons,
-  Icon,
-  Card,
-  CardHead,
-  CardBody,
-  Overlay,
-  Loading,
-}
+export { Nav, SSR, Icons, Icon, Card, CardHead, CardBody, Overlay, Loading }
