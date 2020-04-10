872d7fd090fe414ef7d1fec13bb3e08a3baf6d1c
Barry Low <bearcanrun@gmail.com>
2020-04-10 11:30:35 -0700

    feat(yg-col-api): initial seeds + queries

--- apps/yg-col-api/src/index.ts
+++ apps/yg-col-api/src/index.ts
@@ -1,3 +1,53 @@
 require('dotenv').config()
+import { PrismaClient } from '@prisma/client'
 
-export const noFunc = () => false
+const prisma = new PrismaClient()
+
+const main = async () => {
+  /*
+  await prisma.user.create({
+    data: {
+      name: 'Barry Low',
+      email: 'barry@studiobear.co',
+      profile: {
+        create: {
+          companyName: 'Studiobear Design',
+          phone: { create: { type: 'WORK', number: '7079274658' } },
+        },
+      },
+    },
+  })
+
+  await prisma.collection.create({
+    data: {
+      name: 'Default',
+      works: {
+        create: [
+          {
+            title: 'Floral Still Life with Fruit',
+            medium: 'Oil and Acrylic on Canvas',
+            dimensions: '36 x 36 inches',
+            catalogId: 20001,
+            published: true,
+            price: 6500,
+            category: 'FLORAL',
+          },
+        ],
+      },
+    },
+  })
+  */
+
+  const allUsers = await prisma.user.findMany()
+  const allCollections = await prisma.collection.findMany()
+  const allWorks = await prisma.work.findMany()
+  console.log(allUsers, allCollections, allWorks)
+}
+
+main()
+  .catch(e => {
+    throw e
+  })
+  .finally(async () => {
+    await prisma.disconnect()
+  })
