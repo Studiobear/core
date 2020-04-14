8dbdbaded6503acbac3a838d5d06ceff4ca117a7
Barry Low <bearcanrun@gmail.com>
2020-04-13 20:58:37 -0700

    refactor(yg-col-api): breakout schema to types dir files

--- apps/yg-col-api/src/schema.ts
+++ apps/yg-col-api/src/schema.ts
@@ -1,60 +1,9 @@
 import { nexusPrismaPlugin } from 'nexus-prisma'
-import { makeSchema, objectType } from '@nexus/schema'
-import { getUserId } from './util'
-
-const User = objectType({
-  name: 'User',
-  definition(t) {
-    t.model.id()
-    t.model.name()
-    t.model.email()
-    t.model.role()
-    t.model.profile()
-  },
-})
-
-const Profile = objectType({
-  name: 'Profile',
-  definition(t) {
-    t.model.id()
-    t.model.companyName()
-    t.model.website()
-    t.model.user()
-    t.model.userId()
-  },
-})
-
-const Query = objectType({
-  name: 'Query',
-  definition(t) {
-    t.crud.profile(),
-      t.field('me', {
-        type: 'User',
-        nullable: true,
-        resolve: (parent, args, ctx) => {
-          const userId = getUserId(ctx)
-          if (!userId) {
-            throw new Error('Invalid userId')
-          }
-          return ctx.prisma.user.findOne({
-            where: {
-              id: parseInt(userId),
-            },
-          })
-        },
-      })
-  },
-})
-
-const Mutation = objectType({
-  name: 'Mutation',
-  definition(t) {
-    t.crud.createOneUser()
-  },
-})
+import { makeSchema } from '@nexus/schema'
+import * as types from './types'
 
 export const schema = makeSchema({
-  types: [Query, Mutation, Profile, User],
+  types,
   plugins: [
     nexusPrismaPlugin({
       // Fixes the Cannot find NexusPrisma issue
