8589c3a41afd95514038a749634b577096e2b2c5
Barry Low <bearcanrun@gmail.com>
2020-09-06 12:36:00 -0700

    test(studiobear-site): md-processor separate tests

--- apps/studiobear-site/src/utils/__tests__/md-processor.test.js
+++ apps/studiobear-site/src/utils/__tests__/md-processor.test.js
@@ -4,15 +4,22 @@ import { processor } from '../md-processor'
 const filePath = './src/utils/example.md'
 const postWrap = processor(filePath)
 
-describe('MD Processor', () => {
-  test('should return post title, slug, html and excerpt', async () => {
-    const post = await postWrap(filePath)
+let post
+beforeAll(async () => {
+  post = await postWrap(filePath)
+  // console.log(post)
+  return
+})
 
-    // console.log(post)
+describe('MD Processor', () => {
+  test('returns post title, slug, html and excerpt', () => {
     expect(post).toHaveProperty('title')
     expect(post).toHaveProperty('slug')
     expect(post).toHaveProperty('html')
     expect(post).toHaveProperty('excerpt')
+  })
+
+  test('body is longer than excerpt', () => {
     expect(String(post.html).length > String(post.excerpt).length).toBe(true)
   })
 })
